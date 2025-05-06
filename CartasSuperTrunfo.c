c9818ba15b2afcbd133138080ef0f0239149be6b
jnrxxw <ultraff2014@gmail.com>
2025-05-06 04:07:05 +0000

    0605

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,18 +2,16 @@
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+
 
 int main() {
     
     // informações de uma carta
     
-    char estado ;
-    char codigo [2] ;
+    char estado[3];
+    char codigo [4] ;
     char nomedacidade [100] ;
-    int populaçao ;
+    int populacao ;
     float area ; 
     float pib ;
     int pontos ;
@@ -21,49 +19,51 @@ int main() {
 
     // dados das cartas
 
-    printf ("Estado de A a H" ) ;
+    printf ("Estado de (A a H) : " ) ;
     scanf  ("%s" , estado ) ;
     
 
-    printf ("digite o Código da Carta de de (01 a 03) ")
+    printf ("digite o Código da Carta de de (01 a 03): ");
     scanf ("%s" , codigo ) ;
 
 
-    printf ("digite o nome cidade");
+    printf ("digite o nome cidade: ");
     scanf ("%s" , nomedacidade ) ;
 
     
-    printf ("digite o numero da População") ;
-    scanf ("%d" , &população);
+    printf ("digite o numero da População: ") ;
+    scanf ("%d" , &populacao);
 
 
-    printf ("digite a área da cidade em quilômetros quadrados") ;
+    printf ("digite a área da cidade em quilômetros quadrados: ") ;
     scanf ("%f" , &area ) ;
 
 
-    printf ("digite o PIB");
+    printf ("digite o PIB:");
     scanf ("%f" , &pib);
 
 
-    printf ("digite o Número de Pontos Turísticos") ;
+    printf ("digite o Número de Pontos Turísticos: ") ;
     scanf ("%d" , &pontos ) ; 
 
 
     // Dados da carta
 
-    printf ("Estado : %s" estado ) ;
+    printf("\nDados da Carta\n");
 
-    printf ("Código : %s" codigo ) ;
+    printf ("Estado : %s\n", estado ) ;
 
-    printf ("Nome da Cidade : %s" nomedacidade ) ;
+    printf ("Código : %s\n" , codigo ) ;
 
-    printf ("População : %d" populaçao) ;
+    printf ("Nomedacidade : %s\n" , nomedacidade ) ;
 
-    printf ("Área : %f" area ) ;
+    printf ("População : %d\n" , populacao) ;
 
-    printf ("PIB : %f" pib ) :
+    printf ("Área : %f\n" , area ) ;
 
-    printf ("Número de Pontos Turísticos : %d" pontos ) ;
+    printf ("PIB : %f\n" , pib ) ;
+
+    printf ("Número de Pontos Turísticos : %d\n" , pontos ) ;
 
 
 
